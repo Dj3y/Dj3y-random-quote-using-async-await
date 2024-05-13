84e64089c2f047691a739546810516332b62f41e
Dj3y <dkyamilova@gmail.com>
2024-05-13 13:33:42 +0200

    modification du code JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
-const section = document.getElementById('response');
 const quote = document.getElementById('quote');
 const author = document.getElementById('author');
-const img = document.createElement('img');
-section.appendChild(img);
-function randomQuotes(){
-        fetch('https://thatsthespir.it/api')
-        .then((response) => response.json())
-        .then((data) => {
+const img = document.getElementById('img-author');
+async function randomQuotes(){
+    try{
+        const response = await fetch('https://thatsthespir.it/api');
+        const data = await response.json();
+
             quote.textContent = `${data.quote}`;
             author.textContent =  `${data.author}`;
             img.setAttribute('src', `${data.photo}`);
+            img.setAttribute('alt', `image of ${data.author}`);
 
-        })
-        .catch((error) => {
+        }
+        catch(error) {
             const errorParaph = document.createElement('p');
             section.appendChild(errorParaph);
             errorParaph.textContent = ("Error fetching quote: ", error);
-        })
+        }
 }
+const btn = document.getElementById('generate-quote');
+btn.addEventListener('click', randomQuotes);
 
 randomQuotes();
